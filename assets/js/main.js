e33e2d8c10a2925ec0c28b2f97b52e4d0bfef575
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2024-04-24 08:25:16 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/***************************************
+    Project: Best Web Design 2024
+    * Team: Wingman
+    * Members:
+        1. 23IT199 - Tran Hoang Nhat
+        2. 23IT189 - Nguyen Huu Nhan
+        3. 23IT124 - Huynh Quoc Khanh
+        4. 23IT080 - Trinh Duy Hieu
+    * Website:
+        <website>
+            <Brand>GGWear</Brand>
+            <Type>Fashion</Type> 
+        </website>
+***************************************/
+
+(function() {
+    document.addEventListener('DOMContentLoaded', (e) => {
+        
+    });
+})();
